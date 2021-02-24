816760a2299c39d31d0751bc70649ede1bcf52bf
Matt Calimbas <matthewcalimbas@gmail.com>
2021-02-23 22:02:43 -0800

    major frontend re-work

--- scripts.js
+++ scripts.js
@@ -4,8 +4,44 @@ $(function (){
         $('.nav-list').toggleClass('open');
     });
 
-    AOS.init({
-        easing: 'ease',
-        duration:1000,
-    })
+    $('#hero-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        fade: true,
+        speed: 1000,
+        cssEase: 'linear'
+      });
+
+      $('#trainers-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<a href="#" class="slick-arrow slick-prev">previous</a>',
+        nextArrow: '<a href="#" class="slick-arrow slick-next">next</a>',
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              arrows: false
+            }
+          },
+          {
+            breakpoint: 530,
+            settings: {
+              slidesToShow: 1,
+              arrows: false
+            }
+          }
+        ]
+      });
+
+    AOS.init({})
 });
